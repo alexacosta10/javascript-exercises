fa279a71ed5958ccc517ec610c530a854cfb2673
alexacosta10 <120304139+alexacosta10@users.noreply.github.com>
2024-03-14 12:24:27 -0700

    Passed tests 01-07, Simple and useful functions to be used for future reference

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) 
+{
+  let reversedString = '';
+  for (i = 1; i == string.length; i++)
+  {
+    reversedString = reversedString + string[string.length - i]
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) 
+{
+  newNum = (num - 32) * (5 / 9);
+  newNum = Math.round(newNum * 10) / 10;
+  return newNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) 
+{
+  newNum = (num * (9 / 5) + 32);
+  newNum = Math.round(newNum * 10) / 10;
+  return newNum;
 };
 
 // Do not edit below this line
